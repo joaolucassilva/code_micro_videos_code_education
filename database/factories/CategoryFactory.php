5d5d6d6e1232eafc363b240a717179a68e0c47f2
JoaoLucas <joao.rodrigues@fretebras.com.br>
2021-12-02 19:28:31 -0300

    feat: migration, factory e seeder categories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,11 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Category;
+use App\Models\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->colorName,
+        'description' => rand(1, 10) % 2 === 0 ? $faker->sentence() : null
     ];
 });
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -3,8 +3,13 @@
 use App\Models\Category;
 use Illuminate\Database\Seeder;
 
-class CategorySeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
         factory(Category::class, 100)->create();
