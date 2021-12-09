7fa95142850e91bf317f778f4927738736cd7103
Joao Lucas <luccas332@gmail.com>
2021-12-08 23:33:38 -0300

    test: testes criação model genre

---
+++ tests/Feature/Models/GenreTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Genre;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class GenreTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $model;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->model = new Genre();
+    }
+
+    public function testList()
+    {
+        factory(Genre::class, 1)->create();
+
+        $genres = $this->model->all();
+        $this->assertCount(1, $genres);
+
+        $genreKeys = array_keys($this->model->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id',
+            'name',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ], $genreKeys);
+    }
+
+    public function testCreate()
+    {
+        $genre = $this->model->create(['name' => 'genre_teste_1']);
+        $genre->refresh();
+        $this->assertEquals('genre_teste_1', $genre->name);
+
+        $genre = $this->model->create(['name' => 'genre_teste_1', 'is_active' => false]);
+        $this->assertFalse($genre->is_active);
+
+        $genre = $this->model->create(['name' => 'genre_teste_1', 'is_active' => true]);
+        $this->assertTrue($genre->is_active);
+    }
+}
