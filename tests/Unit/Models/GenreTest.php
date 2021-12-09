4c1d0d17103aae7fb81d81b99308ec280c3d1504
Joao Lucas <luccas332@gmail.com>
2021-12-08 22:52:03 -0300

    test: criação testes para model Genre

---
+++ tests/Unit/Models/GenreTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Genre;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class GenreTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $model;
+
+    protected function setUp(): void
+    {
+        $this->model = new Genre();
+        parent::setUp();
+    }
+
+    public function testFillableAttribute()
+    {
+        $fillable = [
+            'name',
+            'is_active'
+        ];
+
+        $this->assertEquals($fillable, $this->model->getFillable());
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = [
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ];
+
+        foreach ($dates as $date) {
+            $this->assertContains($date, $this->model->getDates());
+        }
+
+        $this->assertCount(count($dates), $this->model->getDates());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+
+        $genresTraits = array_keys(class_uses(Genre::class));
+
+        $this->assertEquals($traits, $genresTraits);
+    }
+
+    public function testCasts()
+    {
+        $casts = ['id' => 'string'];
+
+        $this->assertEquals($casts, $this->model->getCasts());
+    }
+
+    public function testIncrementing()
+    {
+        $this->assertFalse($this->model->incrementing);
+    }
+}
